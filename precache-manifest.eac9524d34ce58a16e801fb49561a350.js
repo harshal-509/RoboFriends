d68f1885712d0c13fe4639f3f3470a39b8301c2c
harshal-509 <sharma.harshal509@gmail.com>
2022-06-02 19:22:18 +0530

    Updates

---
+++ precache-manifest.eac9524d34ce58a16e801fb49561a350.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "000ab38523a2fa511e44",
+    "url": "/RoboFriends/static/css/main.7400bca9.chunk.css"
+  },
+  {
+    "revision": "000ab38523a2fa511e44",
+    "url": "/RoboFriends/static/js/main.000ab385.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/RoboFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "f4629c8dacda5acd072e",
+    "url": "/RoboFriends/static/css/2.8ccb99f0.chunk.css"
+  },
+  {
+    "revision": "f4629c8dacda5acd072e",
+    "url": "/RoboFriends/static/js/2.f4629c8d.chunk.js"
+  },
+  {
+    "revision": "4cb7496509aa3077e2b1",
+    "url": "/RoboFriends/static/js/runtime~main.4cb74965.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0519a53e843f4fb517e95896dc34d49f",
+    "url": "/RoboFriends/index.html"
+  }
+];
